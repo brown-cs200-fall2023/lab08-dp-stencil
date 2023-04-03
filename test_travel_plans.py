c67641a29b4c5be38304bad201d2e3de144b18ed
Nicholas DeMarinis <ndemarinis@wpi.edu>
2023-04-03 11:07:04 -0400

    Added example test.

--- test_travel_plans.py
+++ test_travel_plans.py
@@ -3,6 +3,16 @@ import pytest
 from travel_plans_dp import TravelPlansDP
 from travel_plans_rec import TravelPlansRec
 
+# Here's an example for a basic unit test
+def test_example():
+    # An assertion
+    assert 2 == 2
+
+    # Assertions on lists
+    assert "a" in ["a", "b", "c"]
+    assert "d" not in ["a", "b", "c"]
+    assert len(["a", "b", "c"]) == 3
+
 # TODO: implement more tests
 def test_rec_and_dp_equal():
     stores1 = [1, 5, 2, 2, 4, 3]
